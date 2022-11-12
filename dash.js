dcbcc6b135107fd8878feb31474f51cf6a650b91
Syed Ali Rehan <104094853+mostlyali07@users.noreply.github.com>
2022-11-12 17:47:58 +0500

    update

---
+++ dash.js
@@ -0,0 +1,46 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+import {
+    getAuth,
+    signInWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
+
+import {
+    doc,
+    setDoc,
+    getFirestore,
+    getDoc,
+    collection,
+    query,
+    where,
+    getDocs,
+    addDoc,
+} from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCmUZy2KYQO_UFn1VWdCfVjKbl5ov005Xw",
+    authDomain: "attendance-app-7477.firebaseapp.com",
+    projectId: "attendance-app-7477",
+    storageBucket: "attendance-app-7477.appspot.com",
+    messagingSenderId: "157105142963",
+    appId: "1:157105142963:web:7ea435d8784d518cdc84c6",
+    measurementId: "G-XFLHSGPV2M"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+
+
+const submit_stu = document.getElementById("create_stu");
+
+
+submit_stu.addEventListener("click", (event) => {
+    event.preventDefault()
+    let teachers = document.getElementById("teachers").value;
+    let class_timing = document.getElementById("class_timing").value;
+    addDoc(collection(db, "Students"), {
+        teacher: teachers,
+        class_timing: class_timing
+    });
+});
